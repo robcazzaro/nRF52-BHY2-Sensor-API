4c8dcaef6c1521cccc5ce9f7b024d67132658ccf
robcazzaro <robcazzaro@users.noreply.github.com>
2022-08-30 13:31:19 -0700

    Version 1.0, ready for initial release

--- examples/common/common.h
+++ examples/common/common.h
@@ -1,4 +1,11 @@
 /**
+ *
+ * Copyright (C) 2022 Roberto Cazzaro <https://github.com/robcazzaro>
+ * 
+ * Bosch BHY2 libraries ported to nRF5 SDK (17.1.0)
+ *
+ *
+ *
  * Copyright (c) 2020 Bosch Sensortec GmbH. All rights reserved.
  *
  * BSD-3-Clause
@@ -39,26 +46,23 @@
 #ifndef _COMMON_H_
 #define _COMMON_H_
 
-#include <stdbool.h>
-
 #include "bhy2.h"
-#include "coines.h"
 
-#define BHY260_CS_PIN  COINES_SHUTTLE_PIN_7
-#define BHY260_INT_PIN COINES_SHUTTLE_PIN_21
+#include <ctype.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
 
 char *get_coines_error(int16_t rslt);
 char *get_api_error(int8_t error_code);
 char *get_sensor_error_text(uint8_t sensor_error);
 char *get_sensor_name(uint8_t sensor_id);
 
-void setup_interfaces(bool reset_power, enum bhy2_intf intf);
-void close_interfaces(void);
+void setup_SPI(void);
 int8_t bhy2_spi_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t length, void *intf_ptr);
 int8_t bhy2_spi_write(uint8_t reg_addr, const uint8_t *reg_data, uint32_t length, void *intf_ptr);
 int8_t bhy2_i2c_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t length, void *intf_ptr);
 int8_t bhy2_i2c_write(uint8_t reg_addr, const uint8_t *reg_data, uint32_t length, void *intf_ptr);
 void bhy2_delay_us(uint32_t us, void *private_data);
-bool get_interrupt_status(void);
 
 #endif /* _COMMON_H_ */
