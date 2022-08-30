202b7ef3783bed09e02927ae548d7b16edd32fa9
robcazzaro <robcazzaro@users.noreply.github.com>
2022-08-30 13:32:30 -0700

    Version 1.0, ready for initial release

--- examples/euler/euler.c
+++ examples/euler/euler.c
@@ -1,4 +1,12 @@
 /**
+ *
+ * Copyright (C) 2022 Roberto Cazzaro <https://github.com/robcazzaro>
+ * 
+ * Bosch BHY2 libraries ported to nRF5 SDK (17.1.0)
+ *
+ *
+ *
+ *
  * Copyright (c) 2020 Bosch Sensortec GmbH. All rights reserved.
  *
  * BSD-3-Clause
@@ -36,22 +44,26 @@
  *
  */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdbool.h>
-
 #include "bhy2.h"
 #include "bhy2_parse.h"
+#include "bhy2_hif.h"
+#include "bhy2_defs.h"
 #include "common.h"
 
-#include "bhi260ap/Bosch_APP30_SHUTTLE_BHI260_aux_BMM150.fw.h"
+#include "nrf_delay.h"
+#include "nrf_drv_gpiote.h"
+#include "nrf_gpio.h"
+
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+
+#include "Bosch_SHUTTLE_BHI260_aux_BMM150.fw.h"
+
+#include "nrf_pwr_mgmt.h"
 
 #define WORK_BUFFER_SIZE   2048
-#if defined (PC)
-#define MAX_READ_WRITE_LEN 44
-#else
 #define MAX_READ_WRITE_LEN 256
-#endif
 
 #define EULER_SENSOR_ID    BHY2_SENSOR_ID_ORI_WU
 
@@ -59,20 +71,37 @@ static void parse_euler(const struct bhy2_fifo_parse_data_info *callback_info, v
 static void parse_meta_event(const struct bhy2_fifo_parse_data_info *callback_info, void *callback_ref);
 static void print_api_error(int8_t rslt, struct bhy2_dev *dev);
 
-int main(void)
+static void gpio_init(void);
+static void mems_int_handler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t action);
+
+struct bhy2_dev bhy2;
+uint8_t work_buffer[WORK_BUFFER_SIZE];
+
+int main_euler(void)
 {
     uint8_t product_id = 0;
     uint16_t version = 0;
     int8_t rslt;
-    struct bhy2_dev bhy2;
-    uint8_t work_buffer[WORK_BUFFER_SIZE];
+// moved to global    struct bhy2_dev bhy2;
+// moved to global    uint8_t work_buffer[WORK_BUFFER_SIZE];
     uint8_t hintr_ctrl, hif_ctrl, boot_status;
     uint8_t accuracy; /* Accuracy is reported as a meta event. It is being printed alongside the data */
 
-    setup_interfaces(true, BHY2_SPI_INTERFACE); /* Perform a power on reset */
+    // Setup SPI peripheral for nRF52. See readme for BHI260 connections
+    setup_SPI();
 
     rslt = bhy2_init(BHY2_SPI_INTERFACE, bhy2_spi_read, bhy2_spi_write, bhy2_delay_us, MAX_READ_WRITE_LEN, NULL, &bhy2);
     print_api_error(rslt, &bhy2);
+
+    nrf_gpio_cfg_output(BSP_MEMS_nRESET);   // GPIO is connected to BHI260 RESETN pin
+    nrf_gpio_pin_clear(BSP_MEMS_nRESET);    // BHI260 reset is active  low
+    nrf_delay_us(1);                        // Wait minimum safe time for reset (min 100nsec)
+    nrf_gpio_pin_set(BSP_MEMS_nRESET);      // End of reset sequence
+    // The default protocol used by BHI260 after power-up or RESET is I2C. Pulling the HCSB (Host Chip Select) pin LOW 
+    // at any time switches the host interface (HIF) into SPI mode, where it remains until next RESET or power-on cycle
+    nrf_gpio_cfg_output(BSP_MEMS_CS);   // BHI260 HCSB
+    nrf_gpio_pin_clear(BSP_MEMS_CS);
+    nrf_delay_us(1);
 
     rslt = bhy2_soft_reset(&bhy2);
     print_api_error(rslt, &bhy2);
@@ -83,30 +112,30 @@ int main(void)
     /* Check for a valid product ID */
     if (product_id != BHY2_PRODUCT_ID)
     {
-        printf("Product ID read %X. Expected %X\r\n", product_id, BHY2_PRODUCT_ID);
+        NRF_LOG_INFO("Product ID read %X. Expected %X", product_id, BHY2_PRODUCT_ID);
     }
     else
     {
-        printf("BHI260/BHA260 found. Product ID read %X\r\n", product_id);
+        NRF_LOG_INFO("BHI260/BHA260 found. Product ID read %X", product_id);
     }
 
-    /* Check the interrupt pin and FIFO configurations. Disable status and debug */
-    hintr_ctrl = BHY2_ICTL_DISABLE_STATUS_FIFO | BHY2_ICTL_DISABLE_DEBUG;
+    // Disable status and debug, and temporarily disable interrupts to prevent issues with interrupt-driven code. Page 121 datasheet
+    hintr_ctrl = BHY2_ICTL_DISABLE_STATUS_FIFO | BHY2_ICTL_DISABLE_DEBUG | BHY2_ICTL_DISABLE_FIFO_W | BHY2_ICTL_DISABLE_FIFO_NW | BHY2_ICTL_DISABLE_FAULT | BHY2_ICTL_OPEN_DRAIN;
 
     rslt = bhy2_set_host_interrupt_ctrl(hintr_ctrl, &bhy2);
     print_api_error(rslt, &bhy2);
     rslt = bhy2_get_host_interrupt_ctrl(&hintr_ctrl, &bhy2);
     print_api_error(rslt, &bhy2);
 
-    printf("Host interrupt control\r\n");
-    printf("    Wake up FIFO %s.\r\n", (hintr_ctrl & BHY2_ICTL_DISABLE_FIFO_W) ? "disabled" : "enabled");
-    printf("    Non wake up FIFO %s.\r\n", (hintr_ctrl & BHY2_ICTL_DISABLE_FIFO_NW) ? "disabled" : "enabled");
-    printf("    Status FIFO %s.\r\n", (hintr_ctrl & BHY2_ICTL_DISABLE_STATUS_FIFO) ? "disabled" : "enabled");
-    printf("    Debugging %s.\r\n", (hintr_ctrl & BHY2_ICTL_DISABLE_DEBUG) ? "disabled" : "enabled");
-    printf("    Fault %s.\r\n", (hintr_ctrl & BHY2_ICTL_DISABLE_FAULT) ? "disabled" : "enabled");
-    printf("    Interrupt is %s.\r\n", (hintr_ctrl & BHY2_ICTL_ACTIVE_LOW) ? "active low" : "active high");
-    printf("    Interrupt is %s triggered.\r\n", (hintr_ctrl & BHY2_ICTL_EDGE) ? "pulse" : "level");
-    printf("    Interrupt pin drive is %s.\r\n", (hintr_ctrl & BHY2_ICTL_OPEN_DRAIN) ? "open drain" : "push-pull");
+    NRF_LOG_INFO("Host interrupt control");
+    NRF_LOG_INFO("    Wake up FIFO %s.", (hintr_ctrl & BHY2_ICTL_DISABLE_FIFO_W) ? "disabled" : "enabled");
+    NRF_LOG_INFO("    Non wake up FIFO %s.", (hintr_ctrl & BHY2_ICTL_DISABLE_FIFO_NW) ? "disabled" : "enabled");
+    NRF_LOG_INFO("    Status FIFO %s.", (hintr_ctrl & BHY2_ICTL_DISABLE_STATUS_FIFO) ? "disabled" : "enabled");
+    NRF_LOG_INFO("    Debugging %s.", (hintr_ctrl & BHY2_ICTL_DISABLE_DEBUG) ? "disabled" : "enabled");
+    NRF_LOG_INFO("    Fault %s.", (hintr_ctrl & BHY2_ICTL_DISABLE_FAULT) ? "disabled" : "enabled");
+    NRF_LOG_INFO("    Interrupt is %s.", (hintr_ctrl & BHY2_ICTL_ACTIVE_LOW) ? "active low" : "active high");
+    NRF_LOG_INFO("    Interrupt is %s triggered.", (hintr_ctrl & BHY2_ICTL_EDGE) ? "pulse" : "level");
+    NRF_LOG_INFO("    Interrupt pin drive is %s.", (hintr_ctrl & BHY2_ICTL_OPEN_DRAIN) ? "open drain" : "push-pull");
 
     /* Configure the host interface */
     hif_ctrl = 0;
@@ -121,24 +150,24 @@ int main(void)
     {
         uint8_t sensor_error;
         int8_t temp_rslt;
-        printf("Loading firmware into RAM.\r\n");
+        NRF_LOG_INFO("Loading firmware into RAM.");
 
         rslt = bhy2_upload_firmware_to_ram(bhy2_firmware_image, sizeof(bhy2_firmware_image), &bhy2);
         temp_rslt = bhy2_get_error_value(&sensor_error, &bhy2);
         if (sensor_error)
         {
-            printf("%s\r\n", get_sensor_error_text(sensor_error));
+            NRF_LOG_INFO("%s", get_sensor_error_text(sensor_error));
         }
         print_api_error(rslt, &bhy2);
         print_api_error(temp_rslt, &bhy2);
 
-        printf("Booting from RAM.\r\n");
+        NRF_LOG_INFO("Booting from RAM.");
 
         rslt = bhy2_boot_from_ram(&bhy2);
         temp_rslt = bhy2_get_error_value(&sensor_error, &bhy2);
         if (sensor_error)
         {
-            printf("%s\r\n", get_sensor_error_text(sensor_error));
+            NRF_LOG_INFO("%s", get_sensor_error_text(sensor_error));
         }
         print_api_error(rslt, &bhy2);
         print_api_error(temp_rslt, &bhy2);
@@ -147,13 +176,14 @@ int main(void)
         print_api_error(rslt, &bhy2);
         if ((rslt == BHY2_OK) && (version != 0))
         {
-            printf("Boot successful. Kernel version %u.\r\n", version);
+            NRF_LOG_INFO("Boot successful. Kernel version %u.", version);
         }
 
-        rslt = bhy2_register_fifo_parse_callback(BHY2_SYS_ID_META_EVENT, parse_meta_event, (void*)&accuracy, &bhy2);
-        print_api_error(rslt, &bhy2);
-        rslt = bhy2_register_fifo_parse_callback(BHY2_SYS_ID_META_EVENT_WU, parse_meta_event, (void*)&accuracy, &bhy2);
-        print_api_error(rslt, &bhy2);
+        // Disabled mete event parsing: sometimes too many messages cause problems, can be re-enabled for test
+//        rslt = bhy2_register_fifo_parse_callback(BHY2_SYS_ID_META_EVENT, parse_meta_event, (void*)&accuracy, &bhy2);
+//        print_api_error(rslt, &bhy2);
+//        rslt = bhy2_register_fifo_parse_callback(BHY2_SYS_ID_META_EVENT_WU, parse_meta_event, (void*)&accuracy, &bhy2);
+//        print_api_error(rslt, &bhy2);
         rslt = bhy2_register_fifo_parse_callback(EULER_SENSOR_ID, parse_euler, (void*)&accuracy, &bhy2);
         print_api_error(rslt, &bhy2);
 
@@ -162,9 +192,7 @@ int main(void)
     }
     else
     {
-        printf("Host interface not ready. Exiting\r\n");
-
-        close_interfaces();
+        NRF_LOG_INFO("Host interface not ready. Exiting");
 
         return 0;
     }
@@ -173,26 +201,30 @@ int main(void)
     rslt = bhy2_update_virtual_sensor_list(&bhy2);
     print_api_error(rslt, &bhy2);
 
-    float sample_rate = 100.0; /* Read out data measured at 100Hz */
+    float sample_rate = 1.0; /* Read out data measured at 1Hz. Avoid high rates when using NRF_LOG */
     uint32_t report_latency_ms = 0; /* Report immediately */
     rslt = bhy2_set_virt_sensor_cfg(EULER_SENSOR_ID, sample_rate, report_latency_ms, &bhy2);
     print_api_error(rslt, &bhy2);
     printf("Enable %s at %.2fHz.\r\n", get_sensor_name(EULER_SENSOR_ID), sample_rate);
 
-    while (rslt == BHY2_OK)
+// Nordic specific code to enable GPIOTE
+    hintr_ctrl = BHY2_ICTL_DISABLE_STATUS_FIFO | BHY2_ICTL_DISABLE_DEBUG | BHY2_ICTL_ACTIVE_LOW | BHY2_ICTL_EDGE; // re-enable interrupts. Page 121 datasheet
+    rslt = bhy2_set_host_interrupt_ctrl(hintr_ctrl, &bhy2);
+    print_api_error(rslt, &bhy2);
+
+    gpio_init();
+
+    // Enter main loop. Never exits. Code is interrupt driven from the BHI260, every time data is ready, it's printed by the parse_euler() function
+	// in real use, parse_euler() will load data into variables to be used by the rest of the code
+    for (;;)
     {
-        if (get_interrupt_status())
+        if (NRF_LOG_PROCESS() == false)
         {
-            /* Data from the FIFO is read and the relevant callbacks if registered are called */
-            rslt = bhy2_get_and_process_fifo(work_buffer, WORK_BUFFER_SIZE, &bhy2);
-            print_api_error(rslt, &bhy2);
+            nrf_pwr_mgmt_run();
         }
     }
-
-    close_interfaces();
-
-    return rslt;
 }
+
 
 static void parse_euler(const struct bhy2_fifo_parse_data_info *callback_info, void *callback_ref)
 {
@@ -215,26 +247,53 @@ static void parse_euler(const struct bhy2_fifo_parse_data_info *callback_info, v
 
     if (accuracy)
     {
-        printf("SID: %u; T: %u.%09u; h: %f, p: %f, r: %f; acc: %u\r\n",
+/*        NRF_LOG_INFO("SID: T: %u.%09u; h: %s%d.%02d, p: %s%d.%02d, r: %s%d.%02d; acc: %u",
+               s,
+               ns,
+               NRF_LOG_FLOAT(data.heading * 360.0f / 32768.0f),
+               NRF_LOG_FLOAT(data.pitch * 360.0f / 32768.0f),
+               NRF_LOG_FLOAT(data.roll * 360.0f / 32768.0f),
+               *accuracy);*/
+
+        NRF_LOG_INFO("SID: T: %u.%09u; h: %d, p: %d, r: %d; acc: %u",
+               s,
+               ns,
+               data.heading,
+               data.pitch,
+               data.roll,
+               *accuracy);
+
+
+
+/*        NRF_LOG_INFO("SID: %u; T: %u.%09u; h: %f, p: %f, r: %f; acc: %u",
                callback_info->sensor_id,
                s,
                ns,
                data.heading * 360.0f / 32768.0f,
                data.pitch * 360.0f / 32768.0f,
                data.roll * 360.0f / 32768.0f,
-               *accuracy);
+               *accuracy);*/
     }
     else
     {
-        printf("SID: %u; T: %u.%09u; h: %f, p: %f, r: %f\r\n",
-               callback_info->sensor_id,
+/*        NRF_LOG_INFO("SID: T: %u.%09u; h: %f, p: %f, r: %f",
                s,
                ns,
                data.heading * 360.0f / 32768.0f,
                data.pitch * 360.0f / 32768.0f,
                data.roll * 360.0f / 32768.0f);
+*/
+
+/*        NRF_LOG_INFO("SID: %u; T: %u.%09u; h: %f, p: %f, r: %f",
+               callback_info->sensor_id,
+               s,
+               ns,
+               data.heading * 360.0f / 32768.0f,
+               data.pitch * 360.0f / 32768.0f,
+               data.roll * 360.0f / 32768.0f);*/
     }
 }
+
 
 static void parse_meta_event(const struct bhy2_fifo_parse_data_info *callback_info, void *callback_ref)
 {
@@ -261,61 +320,61 @@ static void parse_meta_event(const struct bhy2_fifo_parse_data_info *callback_in
     switch (meta_event_type)
     {
         case BHY2_META_EVENT_FLUSH_COMPLETE:
-            printf("%s Flush complete for sensor id %u\r\n", event_text, byte1);
+            NRF_LOG_INFO("%s Flush complete for sensor id %u", event_text, byte1);
             break;
         case BHY2_META_EVENT_SAMPLE_RATE_CHANGED:
-            printf("%s Sample rate changed for sensor id %u\r\n", event_text, byte1);
+            NRF_LOG_INFO("%s Sample rate changed for sensor id %u", event_text, byte1);
             break;
         case BHY2_META_EVENT_POWER_MODE_CHANGED:
-            printf("%s Power mode changed for sensor id %u\r\n", event_text, byte1);
+            NRF_LOG_INFO("%s Power mode changed for sensor id %u", event_text, byte1);
             break;
         case BHY2_META_EVENT_ALGORITHM_EVENTS:
-            printf("%s Algorithm event\r\n", event_text);
+            NRF_LOG_INFO("%s Algorithm event", event_text);
             break;
         case BHY2_META_EVENT_SENSOR_STATUS:
-            printf("%s Accuracy for sensor id %u changed to %u\r\n", event_text, byte1, byte2);
+            NRF_LOG_INFO("%s Accuracy for sensor id %u changed to %u", event_text, byte1, byte2);
             if (accuracy)
             {
                 *accuracy = byte2;
             }
             break;
         case BHY2_META_EVENT_BSX_DO_STEPS_MAIN:
-            printf("%s BSX event (do steps main)\r\n", event_text);
+            NRF_LOG_INFO("%s BSX event (do steps main)", event_text);
             break;
         case BHY2_META_EVENT_BSX_DO_STEPS_CALIB:
-            printf("%s BSX event (do steps calib)\r\n", event_text);
+            NRF_LOG_INFO("%s BSX event (do steps calib)", event_text);
             break;
         case BHY2_META_EVENT_BSX_GET_OUTPUT_SIGNAL:
-            printf("%s BSX event (get output signal)\r\n", event_text);
+            NRF_LOG_INFO("%s BSX event (get output signal)", event_text);
             break;
         case BHY2_META_EVENT_SENSOR_ERROR:
-            printf("%s Sensor id %u reported error 0x%02X\r\n", event_text, byte1, byte2);
+            NRF_LOG_INFO("%s Sensor id %u reported error 0x%02X", event_text, byte1, byte2);
             break;
         case BHY2_META_EVENT_FIFO_OVERFLOW:
-            printf("%s FIFO overflow\r\n", event_text);
+            NRF_LOG_INFO("%s FIFO overflow", event_text);
             break;
         case BHY2_META_EVENT_DYNAMIC_RANGE_CHANGED:
-            printf("%s Dynamic range changed for sensor id %u\r\n", event_text, byte1);
+            NRF_LOG_INFO("%s Dynamic range changed for sensor id %u", event_text, byte1);
             break;
         case BHY2_META_EVENT_FIFO_WATERMARK:
-            printf("%s FIFO watermark reached\r\n", event_text);
+            NRF_LOG_INFO("%s FIFO watermark reached", event_text);
             break;
         case BHY2_META_EVENT_INITIALIZED:
-            printf("%s Firmware initialized. Firmware version %u\r\n", event_text, ((uint16_t)byte2 << 8) | byte1);
+            NRF_LOG_INFO("%s Firmware initialized. Firmware version %u", event_text, ((uint16_t)byte2 << 8) | byte1);
             break;
         case BHY2_META_TRANSFER_CAUSE:
-            printf("%s Transfer cause for sensor id %u\r\n", event_text, byte1);
+            NRF_LOG_INFO("%s Transfer cause for sensor id %u", event_text, byte1);
             break;
         case BHY2_META_EVENT_SENSOR_FRAMEWORK:
-            printf("%s Sensor framework event for sensor id %u\r\n", event_text, byte1);
+            NRF_LOG_INFO("%s Sensor framework event for sensor id %u", event_text, byte1);
             break;
         case BHY2_META_EVENT_RESET:
-            printf("%s Reset event\r\n", event_text);
+            NRF_LOG_INFO("%s Reset event", event_text);
             break;
         case BHY2_META_EVENT_SPACER:
             break;
         default:
-            printf("%s Unknown meta event with id: %u\r\n", event_text, meta_event_type);
+            NRF_LOG_INFO("%s Unknown meta event with id: %u", event_text, meta_event_type);
             break;
     }
 }
@@ -324,12 +383,42 @@ static void print_api_error(int8_t rslt, struct bhy2_dev *dev)
 {
     if (rslt != BHY2_OK)
     {
-        printf("%s\r\n", get_api_error(rslt));
+        NRF_LOG_INFO("%s", get_api_error(rslt));
         if ((rslt == BHY2_E_IO) && (dev != NULL))
         {
-            printf("%s\r\n", get_coines_error(dev->hif.intf_rslt));
+            NRF_LOG_INFO("%s", get_api_error(dev->hif.intf_rslt));
             dev->hif.intf_rslt = BHY2_INTF_RET_SUCCESS;
         }
         exit(0);
     }
+}
+
+static void gpio_init(void)
+{
+    ret_code_t err_code;
+
+    if(!nrf_drv_gpiote_is_init())
+    {
+        err_code = nrf_drv_gpiote_init();
+        APP_ERROR_CHECK(err_code);
+    }
+
+    nrf_drv_gpiote_in_config_t mems_int;
+    mems_int.sense = GPIOTE_CONFIG_POLARITY_HiToLo;
+    mems_int.pull = NRF_GPIO_PIN_PULLUP;
+    mems_int.hi_accuracy = true;
+    mems_int.is_watcher = false; //Don't change this
+    mems_int.skip_gpio_setup = false; //Don't change this
+
+    err_code = nrf_drv_gpiote_in_init((nrfx_gpiote_pin_t)BSP_MEMS_INT, &mems_int, mems_int_handler);
+    APP_ERROR_CHECK(err_code);
+
+    nrf_drv_gpiote_in_event_enable(BSP_MEMS_INT, true);
+}
+
+static void mems_int_handler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t action)
+{
+    int8_t rslt;
+    rslt = bhy2_get_and_process_fifo(work_buffer, WORK_BUFFER_SIZE, &bhy2);
+    print_api_error(rslt, &bhy2);
 }
